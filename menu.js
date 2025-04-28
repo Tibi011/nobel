92fdfc8df659cd63e866d6f138d2bb5c15f4a08a
Tibi011 <megsztil@gmail.com>
2025-04-28 08:21:39 +0200

    new

--- menu.js
+++ menu.js
@@ -1,18 +1,23 @@
-document.getElementById("menu").innerHTML=`<nav class="navbar navbar-expand-sm bg-light">
+document.getElementById("menu").innerHTML=`
 
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <!-- Links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Összes díj</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-    </ul>
+    <a class="navbar-brand" href="https://hu.wikipedia.org/wiki/Nobel-d%C3%ADj">Nobel-díj</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">Összes díj</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
   </div>
-
 </nav>`
